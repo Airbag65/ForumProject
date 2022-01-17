eca19c849aea41b0bff2236e8309db675cec291f
Anton <normananton03@gmail.com>
2022-01-17 22:50:59 +0100

    Påbörjat PHP projekt

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
 <?php
+require_once "../vendor/autoload.php";
+
+$filename = "/home/anton/Desktop/filer/Forum/ForumProject/database";
+$dsn = "sqlite:$filename";
+
+$pdo = new PDO($dsn);
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+$pdo->exec("PRAGMA foreign_keys = ON");
+
+
+echo "Hello World!";
